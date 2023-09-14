00474b6b5f76e706aa0cea8960758649117da8f0
Scalro <rotichcaleb200@gmail.com>
2023-09-14 08:10:55 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
-function App() {
-  return (
-    <div >
-     Learn React
+function App(props) {
+    return (
+    <div>
+      <Navbar/>
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contacts />
     </div>
   );
 }
-
 export default App;
